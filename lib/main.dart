fded7c8e875450db13ee3fda2f8a730d4d6495a0
Leo Kosta <kostaleonard@gmail.com>
2022-05-18 11:47:19 -0400

    Added flutter quickstart part 2
    Signed-off-by: Leo Kosta <kostaleonard@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -15,16 +15,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Startup name generator',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Startup name generator'),
+        title: 'Startup name generator',
+        theme: ThemeData(
+          appBarTheme: const AppBarTheme(
+              backgroundColor: Colors.indigo, foregroundColor: Colors.white),
         ),
-        body: const Center(
-          child: RandomWords(),
-        ),
-      ),
-    );
+        home: const RandomWords());
   }
 }
 
@@ -37,21 +33,62 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18);
+
+  void _pushSaved() {
+    Navigator.of(context).push(MaterialPageRoute<void>(builder: (context) {
+      final tiles = _saved.map((pair) {
+        return ListTile(title: Text(pair.asPascalCase, style: _biggerFont));
+      });
+      final divided = tiles.isNotEmpty
+          ? ListTile.divideTiles(tiles: tiles, context: context).toList()
+          : <Widget>[];
+      return Scaffold(
+        appBar: AppBar(title: const Text('Saved suggestions')),
+        body: ListView(children: divided),
+      );
+    }));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context, i) {
-          if (i.isOdd) return const Divider();
-          final index = i ~/ 2;
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return ListTile(
-              title:
-                  Text(_suggestions[index].asPascalCase, style: _biggerFont));
-        });
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Startup name generator'),
+          actions: [
+            IconButton(
+                onPressed: _pushSaved,
+                icon: const Icon(Icons.list),
+                tooltip: 'Saved suggestions')
+          ],
+        ),
+        body: ListView.builder(
+            padding: const EdgeInsets.all(16.0),
+            itemBuilder: (context, i) {
+              if (i.isOdd) return const Divider();
+              final index = i ~/ 2;
+              if (index >= _suggestions.length) {
+                _suggestions.addAll(generateWordPairs().take(10));
+              }
+              final alreadySaved = _saved.contains(_suggestions[index]);
+              return ListTile(
+                title:
+                    Text(_suggestions[index].asPascalCase, style: _biggerFont),
+                trailing: Icon(
+                    alreadySaved ? Icons.favorite : Icons.favorite_border,
+                    color: alreadySaved ? Colors.red : null,
+                    semanticLabel: alreadySaved ? 'Remove from saved' : 'Save'),
+                onTap: () {
+                  setState(() {
+                    if (alreadySaved) {
+                      _saved.remove(_suggestions[index]);
+                    } else {
+                      _saved.add(_suggestions[index]);
+                    }
+                  });
+                },
+              );
+            }));
   }
 }
