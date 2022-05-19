9266246938d144482d0bac764609375e93f49587
Leo Kosta <kostaleonard@gmail.com>
2022-05-19 16:07:05 -0400

    Added CI
    Signed-off-by: Leo Kosta <kostaleonard@gmail.com>

---
+++ lib/random_words.dart
@@ -0,0 +1,73 @@
+//Contains the RandomWords widget.
+
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
+  final _biggerFont = const TextStyle(fontSize: 18);
+
+  void _pushSaved() {
+    Navigator.of(context).push(MaterialPageRoute<void>(builder: (context) {
+      final tiles = _saved.map((pair) {
+        return ListTile(title: Text(pair.asPascalCase, style: _biggerFont));
+      });
+      final divided = tiles.isNotEmpty
+          ? ListTile.divideTiles(tiles: tiles, context: context).toList()
+          : <Widget>[];
+      return Scaffold(
+        appBar: AppBar(title: const Text('Saved suggestions')),
+        body: ListView(children: divided),
+      );
+    }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Startup name generator'),
+          actions: [
+            IconButton(
+                onPressed: _pushSaved,
+                icon: const Icon(Icons.list),
+                tooltip: 'Saved suggestions')
+          ],
+        ),
+        body: ListView.builder(
+            padding: const EdgeInsets.all(16.0),
+            itemBuilder: (context, i) {
+              if (i.isOdd) return const Divider();
+              final index = i ~/ 2;
+              if (index >= _suggestions.length) {
+                _suggestions.addAll(generateWordPairs().take(10));
+              }
+              final alreadySaved = _saved.contains(_suggestions[index]);
+              return ListTile(
+                title:
+                    Text(_suggestions[index].asPascalCase, style: _biggerFont),
+                trailing: Icon(
+                    alreadySaved ? Icons.favorite : Icons.favorite_border,
+                    color: alreadySaved ? Colors.red : null,
+                    semanticLabel: alreadySaved ? 'Remove from saved' : 'Save'),
+                onTap: () {
+                  setState(() {
+                    if (alreadySaved) {
+                      _saved.remove(_suggestions[index]);
+                    } else {
+                      _saved.add(_suggestions[index]);
+                    }
+                  });
+                },
+              );
+            }));
+  }
+}
