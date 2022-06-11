5685ad647ee6b8e00f576e3ba842999da143a70d
Leo Kosta <kostaleonard@gmail.com>
2022-06-11 15:38:41 -0400

    Added post ordering test
    Signed-off-by: Leo Kosta <kostaleonard@gmail.com>

--- test/chat_test.dart
+++ test/chat_test.dart
@@ -10,7 +10,8 @@ void main() {
     expect(find.byType(TextField), findsOneWidget);
   });
 
-  testWidgets('Pressing send button displays new message', (WidgetTester tester) async {
+  testWidgets('Pressing send button displays new message',
+      (WidgetTester tester) async {
     await tester.pumpWidget(const MaterialApp(home: ChatWidget()));
     await tester.enterText(find.byType(TextField), 'sample post');
     await tester.tap(find.byType(FloatingActionButton));
@@ -20,7 +21,8 @@ void main() {
     expect(tester.firstWidget(textFind), isA<Text>());
   });
 
-  testWidgets('Pressing send button clears text field', (WidgetTester tester) async {
+  testWidgets('Pressing send button clears text field',
+      (WidgetTester tester) async {
     await tester.pumpWidget(const MaterialApp(home: ChatWidget()));
     final textFieldFind = find.byType(TextField);
     await tester.enterText(textFieldFind, 'sample post');
@@ -30,7 +32,8 @@ void main() {
     expect(textField.controller?.text, isEmpty);
   });
 
-  testWidgets('Pressing enter displays new message', (WidgetTester tester) async {
+  testWidgets('Pressing enter displays new message',
+      (WidgetTester tester) async {
     await tester.pumpWidget(const MaterialApp(home: ChatWidget()));
     await tester.enterText(find.byType(TextField), 'sample post');
     await tester.testTextInput.receiveAction(TextInputAction.done);
@@ -50,9 +53,17 @@ void main() {
     expect(textField.controller?.text, isEmpty);
   });
 
-  //TODO test that we dispose resources properly, e.g., disposing the text editing controller
-
-  //TODO add tests for appending posts
-
-  //TODO add test for displaying posts in the correct order
+  testWidgets('Posts are displayed with most recent at the bottom',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(home: ChatWidget()));
+    await tester.enterText(find.byType(TextField), 'first post');
+    await tester.tap(find.byType(FloatingActionButton));
+    await tester.pump();
+    await tester.enterText(find.byType(TextField), 'second post');
+    await tester.tap(find.byType(FloatingActionButton));
+    await tester.pump();
+    final firstPostCenter = tester.getCenter(find.text('first post'));
+    final secondPostCenter = tester.getCenter(find.text('second post'));
+    expect(firstPostCenter.dy, lessThan(secondPostCenter.dy));
+  });
 }
