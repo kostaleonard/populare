3462da06b246c0df3d3072f77dfe9ee820a89cc4
Leo Kosta <kostaleonard@gmail.com>
2022-07-20 17:02:31 -0400

    Closes #17
    Signed-off-by: Leo Kosta <kostaleonard@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,14 @@
 //Runs the app.
 
-import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:populare/chat.dart';
 import 'package:populare/chat_repository.dart';
 
 void main() {
-  print('Starting up populare app (stdout)');
-  print('dbProxyUri is http://192.168.64.3:30120/db-proxy');
-  print('base: ${Uri.base.authority}');
-  log('Starting up populare app');
-  //runApp(PopulareApp(chatWidget: ChatWidget(chatRepository: ChatRepository(dbProxyUri: 'http://192.168.64.3:30120/db-proxy'))));
-  runApp(PopulareApp(chatWidget: ChatWidget(chatRepository: ChatRepository(dbProxyUri: 'http://${Uri.base.authority}/db-proxy'))));
+  runApp(PopulareApp(
+      chatWidget: ChatWidget(
+          chatRepository: ChatRepository(
+              dbProxyUri: 'http://${Uri.base.authority}/db-proxy'))));
 }
 
 class PopulareApp extends StatelessWidget {
